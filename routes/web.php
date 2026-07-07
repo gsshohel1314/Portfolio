d0c22ec1bc8cb742b5ae60477c0ef58bff394e18
gsshohel1314 <gsshohel1314@gmail.com>
2026-07-07 09:44:29 +0600

    refactor: replace welcome page with JSON api status response

--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,9 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return response()->json([
+        'app' => 'Shohel Rana Portfolio API Backend',
+        'status' => 'Running',
+        'version' => '1.0.0'
+    ]);
 });
